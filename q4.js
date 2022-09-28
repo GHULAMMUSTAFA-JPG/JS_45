ca149899102e6610f787302b71a3da2ba24e2006
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-28 15:06:39 +0500

    q4.js

---
+++ q4.js
@@ -0,0 +1,9 @@
+
+// Q3 and Q4 both are same and theres not much difference between so solution is same
+
+
+const authorName = "Prophit Muhammed (PBUH)"
+const quote = "The strongest amongest you is the one who controls his anger."
+
+
+ console.log(`${authorName} once said, “${quote}”`)
