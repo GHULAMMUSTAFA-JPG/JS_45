421ad8a085067f6b673cdcf970d8c4ab27f259a4
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-28 20:25:02 +0500

    Update q6.js

--- q6.js
+++ q6.js
@@ -1,4 +1,4 @@
-console.log(5+3);
-console.log(10-2);
-console.log(2*4);
-console.log(16/2);
+console.log(5+3);  // Addition
+console.log(10-2); //Subtraction
+console.log(2*4);  //Multiplication
+console.log(16/2); //Division
