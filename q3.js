aeb85579c66a900ec0394ac0950688632b458d36
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-28 14:34:33 +0500

    Create q3.js
    quotation

---
+++ q3.js
@@ -0,0 +1,11 @@
+// Famous Quote: Find a quote from a famous person you admire.
+// Print the quote and the name of its author. Your output should look something like the following,
+// including the quotation marks:
+// Albert Einstein once said, “A person who never made a mistake never tried anything new.”
+
+
+const authorName = "Prophit Muhammed (PBUH)"
+const quote = "The strongest amongest you is the one who controls his anger."
+
+
+ console.log(`${authorName} once said, “${quote}”`)
