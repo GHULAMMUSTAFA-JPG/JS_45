af6897af280227767a51ceabe1365b22e072e7cf
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-28 21:38:07 +0500

    q8.js

---
+++ q8.js
@@ -0,0 +1,13 @@
+
+
+let str = ["Ali","Ahmed","Asad","Amir","Arham"]
+
+for(const i of str ){
+    console.log(i)
+}
+console.log("\n")
+console.log("\n")
+for(let i=0 ;i<str.length;i++){
+    console.log(str[i])
+    
+}
