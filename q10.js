8e24cd4a7633304fc332c1d23e8a1694510fbe5d
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-28 21:38:37 +0500

    q10.js

---
+++ q10.js
@@ -0,0 +1,11 @@
+// Think of your favorite mode of transportation,
+// such as a motorcycle or a car, and make a list 
+//that stores several examples. 
+// Use your list to print a 
+// series of statements about these items, such as
+//  “I would like to own a Honda motorcycle.
+let str = ["Bicycle","Motorcycle","Car","Rickshaw","Tanga","Train","Plane"]
+
+for(const i of str ){
+    console.log(`"I would like to own a ${i}."`)
+}
