e544d0b2ee459ff56aeb21e835225b160b04cc70
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-28 14:21:38 +0500

    Create q2.js
    Q2-3 cases for person's name

---
+++ q2.js
@@ -0,0 +1,29 @@
+
+let personName = 'Ghulam Mustafa'
+
+// case1
+
+let Name = personName.toLowerCase();
+console.log(Name)
+
+// case2
+
+Name=personName.toUpperCase()
+console.log(Name)
+
+
+
+// case3
+
+Name = personName.toLowerCase().split(" ")
+
+const titlecase = (Name) => {
+
+    for(const index in Name){
+        Name[index]=Name[index].charAt(0).toUpperCase()+Name[index].slice(1)
+    }
+    console.log( Name.join(' ') )
+
+}
+
+titlecase(Name)
