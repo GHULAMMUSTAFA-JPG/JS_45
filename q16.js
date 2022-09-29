be2537716bc600d79b7bccb9ff50d622125a5f7b
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-29 20:49:25 +0500

    q16.js

---
+++ q16.js
@@ -0,0 +1,4 @@
+console.log("print a message indicating the number of people you are inviting to dinner.")
+console.log("At the end in program the list was reduced to 2 people and than these 2 was also removed")
+console.log("________________________________________________")
+console.log("Only two people are invited on dinner")
