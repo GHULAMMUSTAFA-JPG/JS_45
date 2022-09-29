4bf9119154fcd358436d867213807e856e9420a0
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-29 20:49:53 +0500

    q19.js

---
+++ q19.js
@@ -0,0 +1,8 @@
+const list = ['a','b','c','d','e','f']
+
+
+console.log(list[9]) // error about index out of bound as undefined 
+console.log(typeof(list[9])) // type of undefined is nothing as it shows nothing
+console.log(list[5]) // resolved the error
+
+
