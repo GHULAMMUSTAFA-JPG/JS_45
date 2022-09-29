b64af71b868408ec1c57aa68285deb9b820f2731
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-29 21:26:52 +0500

    Create q20.js

---
+++ q20.js
@@ -0,0 +1,68 @@
+
+let car = "subaru"
+
+
+console.log("IS car ==   'subaru'?  | predict true")
+console.log("Is car !=   'prosche'? | predict true")
+console.log("is car ===  'subaru'?  | predict true")
+console.log("is car !==  'prosche'? | predict true")
+console.log(" 5 > 2 ?|predict true")
+console.log("is car !== 'subaru'? | predict false")
+console.log(" 6 >= 7?|predict false")
+console.log(" 5 < 2 ?|predict false")
+console.log(" 5==='proshe' ?|predict true")
+console.log("6<=5?|predict true")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
