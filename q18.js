7325a7361a26b4051d81c9b1b0c1846dfe047805
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-29 20:49:44 +0500

    q18.js

---
+++ q18.js
@@ -0,0 +1,10 @@
+const obj = new Object()
+obj.Student1 = "Ghulam"
+obj.Student2= "Mustafa"
+obj.Student3 = "Asadullah"
+obj.Student4 = "Ahmed"
+obj.Student5 = "Mujtaba"
+console.log(obj.Student1)
+
+
+
