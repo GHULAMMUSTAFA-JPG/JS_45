fa0b9785d30699d1e8dcddf26d1e5b21dd484d2e
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-29 20:49:17 +0500

    q15.js

---
+++ q15.js
@@ -0,0 +1,44 @@
+let array = ["Makkah","Madina","Lahore","Islamabad","Karachi"]
+console.log(array)
+let array2 = array.slice().sort()
+console.log(array2)
+console.log(array)
+
+//reverse order
+console.log("Reverse order")
+console.log(array.reverse())
+console.log("re-reversing the list")
+console.log(array.reverse())
+console.log("Sorting the original array")
+console.log(array.sort())
+console.log("Reverse Sorting the original array")
+console.log(array.reverse())
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
