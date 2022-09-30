1f1dafd75b9e473485ba821194f2da017218d746
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-30 15:50:59 +0500

    Create 31.js

---
+++ 31.js
@@ -0,0 +1,27 @@
+let pizza_loop = ['Neapolitan Pizza','Chicago Pizza','New York-Style Pizza']
+
+
+for(const i of pizza_loop){
+    console.log(`I like ${i}`)
+}
+
+
+console.log('I like pizza as it tastes good \nand the other thing i like pizza is its shape \nI really love pizza!')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
