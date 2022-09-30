266a558d1599c4806179b7496bcc4a243a03821b
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-30 15:49:38 +0500

    Create q22.js

---
+++ q22.js
@@ -0,0 +1,16 @@
+let alien_color = "Yellow"
+if(alien_color==="Green"){
+    //console.log("color of alien is green")
+}else if(alien_color === 'Yellow'){
+    console.log("Player hs earned 5 points")
+}
+
+
+
+
+let alienColor = "Green"
+if(alienColor ==="Green"){
+    //console.log("color of alien is green")
+}else if(alienColor === 'Yellow'){
+    console.log("Player hs earned 5 points")
+}
