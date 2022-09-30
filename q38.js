78344ee3a1611e682e1ec1cba29717ef3cea54f9
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-10-01 01:12:37 +0500

    Create q38.js

---
+++ q38.js
@@ -0,0 +1,39 @@
+let magicians_names = ['Magician1','Magician2','Magician3']
+show_magician = (arr) => {
+    for(const i of arr)
+        console.log(`${i}`)
+}
+show_magician(magicians_names)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
