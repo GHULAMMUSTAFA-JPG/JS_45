6c8abaf86fbc3e4c924d54c15069bbf0deadecad
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-10-01 01:12:25 +0500

    Create q35.js

---
+++ q35.js
@@ -0,0 +1,37 @@
+function describe_city(city,country='Pakistan'){
+    console.log(`${city} is in ${country}`)
+}
+
+
+describe_city('lahore',)
+describe_city('karachi',)
+describe_city('rawalpindi',)
+describe_city('Islamabad','Iran')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
