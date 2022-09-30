3d0ed20d78bdcde7d75de4707cf71f73d28b2afb
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-10-01 01:12:20 +0500

    Create 34.js

---
+++ 34.js
@@ -0,0 +1,18 @@
+function make_shirt(size='large',text='I love javascripts'){
+    console.log(`This is ${size}  ${text}`)
+}
+
+
+make_shirt('large shirt',)
+make_shirt('medium',)
+make_shirt('small','text')
+
+
+
+
+
+
+
+
+
+
