de08b1a8a66c05bdff67fab46489b03e592b236d
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-30 15:49:56 +0500

    Create q24.js

---
+++ q24.js
@@ -0,0 +1,29 @@
+let alien_color = "Green"
+if(alien_color === "Green"){
+    console.log("the player just earned 5 points for shooting the alien.")
+}else if(alien_color !== "Green"){
+    console.log("the player just earned 10 points.")
+}else if(alien_color === "Red"){
+    console.log("the player just earned 15 points.")
+}
+
+let alienColor = "Yellow";
+
+if(alienColor === "Green"){
+    console.log("the player just earned 5 points for shooting the alien.")
+}else if(alienColor === "Yellow"){
+    console.log("the player just earned 10 points.")
+}else if(alienColor === "Red"){
+    console.log("the player just earned 15 points.")
+}
+
+
+let AlienColor = "Red";
+
+if(AlienColor === "Green"){
+    console.log("the player just earned 5 points for shooting the alien.")
+}else if(AlienColor === "Yellow"){
+    console.log("the player just earned 10 points.")
+}else if(AlienColor === "Red"){
+    console.log("the player just earned 15 points.")
+}
