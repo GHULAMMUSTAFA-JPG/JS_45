91ff68621b815f51fccff838664b4847165f4814
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-10-01 01:12:13 +0500

    Create 32.js

---
+++ 32.js
@@ -0,0 +1,7 @@
+let animal = ['Deer','Lama','Goat']
+
+for(const i of animal){
+    console.log(`A ${i} runs fast sometimes much faster than any Human`)
+}
+console.log("These three animals are herbivors")
+
