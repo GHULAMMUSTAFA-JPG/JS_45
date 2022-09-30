8afbcbd33fbf240d0dc23b787e21948d18cc10a1
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-30 15:49:35 +0500

    Create q21.js

---
+++ q21.js
@@ -0,0 +1,72 @@
+let x='adad';let y ='aaasdfg' 
+console.log(x!==y)
+
+// on strings
+console.log(" aaa == aaa   | true ")
+console.log(" aaa === aaaa | false")
+console.log(" aaa != aaaa  | true ")
+console.log(" aaa == aaaa  | false")
+console.log(" aaa !== aaaa | true ")
+console.log(3 <  4)
+console.log(3 >  4)
+console.log(4 <= 3)
+console.log(3 >= 4)
+console.log("aaa"==='aaa' && 3===3)
+console.log('aaa'==='aaa' || 3===3)
+let a = [9,8,7,6,5,4,3,2,1,0]
+const i = a.find((item) => typeof item === 'number')
+console.log(i)
+const j = a.find((item) => typeof item === 'String')
+console.log(j)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
