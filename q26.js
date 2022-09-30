c7b06e0b51d2d3acace37d3270174da09a6594b2
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-30 15:50:08 +0500

    Create q26.js

---
+++ q26.js
@@ -0,0 +1,26 @@
+let favorite_fruits = ["Apple","Banana","Orange"]
+
+
+if(favorite_fruits[0] ===  'Apple'){
+    console.log("You really like Apples!")
+}  
+if(favorite_fruits[1] === "Banana" ){
+    console.log("You really like bananas!")
+}
+if(favorite_fruits[2] === 'Orange'){
+    console.log("You really like Oranges!")
+}
+if(favorite_fruits[1] !== 'Apple'){
+    console.log("You really don't like Apples!")
+}
+if(favorite_fruits[0] !== 'Orange'){
+    console.log("You really don't like Orangess!")
+}
+
+
+
+
+
+
+
+
