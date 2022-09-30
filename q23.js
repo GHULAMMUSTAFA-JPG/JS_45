309e25a18f2a754a79c71a7a00149839e89d8540
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-30 15:49:52 +0500

    Create q23.js

---
+++ q23.js
@@ -0,0 +1,14 @@
+let alien_color = "Green"
+if(alien_color === "Green"){
+    console.log("the player just earned 5 points for shooting the alien.")
+}else if(alien_color !== "Green"){
+    console.log("the player just earned 10 points.")
+}
+
+let alienColor = "Yellow";
+
+if(alienColor === "Green"){
+    console.log("the player just earned 5 points for shooting the alien.")
+}else if(alienColor === "Yellow"){
+    console.log("the player just earned 10 points.")
+}
