c5b248c7ea285fe25695afcaab04cacc7a05cc65
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-10-01 01:12:33 +0500

    Create q37.js

---
+++ q37.js
@@ -0,0 +1,72 @@
+make_album = (artist_name,album_title,no_of_tracks) =>{
+    let obj = {
+        artist : artist_name,
+        album : artist_name ,
+        tracks : no_of_tracks
+    }
+    return obj
+}
+
+console.log(make_album('Atif Aslam','Tu Jane Na'))
+
+console.log(make_album( 'abrar-ul-haq','Billo De Ghar',3))//that includes the number of tracks on an album
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
