23e0a659aab5dfd40a826a4c364df541c2cb82bd
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-10-01 01:12:49 +0500

    Create q41.js

---
+++ q41.js
@@ -0,0 +1,19 @@
+a = (arr) =>{
+    console.log(` The sandwich that is ordered contains items that has ordered to be in sandwich are ${arr}`)
+}
+
+
+a(['Tomato','Onion','Ketchup','Mayonnaise','Cheese','Taste Salt','Black pepper','Mustard'])
+
+
+
+
+
+
+
+
+
+
+
+
+
