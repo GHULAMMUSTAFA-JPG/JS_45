4447dbbb74f28ff4f62b183b0f73d2e1e9a25daf
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-09-30 15:49:59 +0500

    Create q25.js

---
+++ q25.js
@@ -0,0 +1,15 @@
+let age = 13
+
+if(age === 2){
+    console.log("the person is a baby")    
+}else if(age <=4 ){
+    console.log(" the person is a toddler.")    
+}else if(age <=13){
+    console.log("the person is a kid.")    
+}else if(age <= 20){
+    console.log("the person is a teenager.")    
+}else if(age <= 65){
+    console.log("the person is an adult.")    
+}else if(age >= 65 ){
+    console.log("the person is an elder")    
+}
