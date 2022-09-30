22769ada5d321ae7f2aff1fadffedda7640a19a3
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-10-01 01:12:16 +0500

    Create 33.js

---
+++ 33.js
@@ -0,0 +1,22 @@
+function make_shirt(size,text){
+    console.log(`This is ${argu} shirt ${text}`)
+}
+
+
+make_shirt('large','text')
+make_shirt('medium','text')
+make_shirt('small','text')
+make_shirt('extra small','text')
+make_shirt('extra large','text')
+make_shirt('extra extra large','text')
+
+
+
+
+
+
+
+
+
+
+
