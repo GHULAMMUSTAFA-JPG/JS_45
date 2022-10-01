553141e5f37d2642291437c3340edc5fcf6d1ad8
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-10-01 15:14:36 +0500

    Update q42.js

--- q42.js
+++ q42.js
@@ -1,7 +1,14 @@
 a= (manufacturer_name,car_name) =>{
-    let car={}
+    let car={
+        manufacturer_name : manufacturer_name , 
+        car_name : car_name
+    }
+    return car
 }
 
+
+console.log(a('Honda',"City"))
+console.log(a('Honda',"City",'Black'))
 
 
 
