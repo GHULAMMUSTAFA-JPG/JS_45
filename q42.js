7ea5a739be3c46f4eba462742936661404c1c706
Ghulam Mustafa <60027443+GHULAMMUSTAFA-JPG@users.noreply.github.com>
2022-10-02 15:55:11 +0500

    Update q42.js

--- q42.js
+++ q42.js
@@ -1,14 +1,66 @@
-a= (manufacturer_name,car_name) =>{
-    let car={
-        manufacturer_name : manufacturer_name , 
-        car_name : car_name
-    }
-    return car
+array_to_Object = array => {
+  let obj = {}
+  for (let i = 0; i < array.length; ++i)
+    if (array[i] !== undefined) obj[i] = array[i]
+  return obj
 }
 
+console.log(array_to_Object(['Honda','City']))
 
-console.log(a('Honda',"City"))
-console.log(a('Honda',"City",'Black'))
+console.log(array_to_Object(['Honda','City','Red']))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// a= (manufacturer_name,car_name) =>{
+//     let car={
+//         manufacturer_name : manufacturer_name , 
+//         car_name : car_name
+//     }
+//     return car
+// }
+
+
+// console.log(a('Honda',"City"))
+// console.log(a('Honda',"City",'Black'))
 
 
 
